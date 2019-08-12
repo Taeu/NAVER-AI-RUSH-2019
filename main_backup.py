04d46c88a0312426e61eef5c8743fdf55e56ad9d
JongHyun Choi <excelsiorcjh@gmail.com>
2019-08-12 23:49:32 +0900

    OctConv 추가

--- main_backup.py
+++ main_backup.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 import numpy as np
 import argparse
 import pathlib
-from model import Baseline, Resnet
+from model import Baseline, Resnet, OctResNet, Bottleneck
 import nsml
 import pandas as pd
 import torchvision.models as models
@@ -14,7 +14,8 @@ from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from dataloader import train_dataloader
 from dataloader import AIRushDataset
-from octconv import OctResNet, Bottleneck   ### <--- octconv.py 파일에서 필요한 클래스 import
+
+from resnext import *
 
 def to_np(t):
     return t.cpu().detach().numpy()
@@ -80,7 +81,7 @@ if __name__ == '__main__':
     parser.add_argument('--gpu_num', type=int, nargs='+', default=[0])
     parser.add_argument('--resnet', default=False)
     parser.add_argument('--resnext', default=False)
-    parser.add_argument('--octconv', default=True)   ### <--- octconv argument 부분!!
+    parser.add_argument('--octconv', default=True)
     parser.add_argument('--hidden_size', type=int, default=256)
     parser.add_argument('--output_size', type=int, default=350) # Fixed
     parser.add_argument('--epochs', type=int, default=30)
@@ -97,14 +98,16 @@ if __name__ == '__main__':
         print("Resnet Model Loaded")
         assert args.input_size == 224
         model = Resnet(args.output_size)
-    elif args.octconv:  # <-- args가 OctConv 일 경우
+    elif args.octconv:
         print("OctConv Model Loaded")
         assert args.input_size == 224
         model = OctResNet(Bottleneck, [3, 4, 6, 3], num_classes=args.output_size)
+        # model = OctCNN(args.output_size)
     elif args.resnext:
         print("ResNext Model Load")
         assert args.input_size == 224
         model = resnext50(num_classes=args.output_size)
+        # model = torch.hub.load('pytorch/vision', 'resnext50_32x4d', pretrained=True)
     else:
         print("Baseline Model Loaded")
         model = Baseline(args.hidden_size, args.output_size)
@@ -160,3 +163,123 @@ if __name__ == '__main__':
                 "train__Loss": total_loss/len(dataloader.dataset),
                 "train__Accuracy": total_correct/len(dataloader.dataset),
                 })
+
+# import os
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import torch.optim as optim
+# import numpy as np
+# import argparse
+# import pathlib
+# from model import Baseline, Resnet
+# import nsml
+# import pandas as pd
+# from torchvision import transforms
+# from torch.utils.data import Dataset, DataLoader
+# from dataloader import train_dataloader
+# from dataloader import AIRushDataset
+
+# from resnext import *
+
+# #from torchsummary import summary
+
+# def to_np(t):
+#     return t.cpu().detach().numpy()
+
+# def bind_model(model_nsml):
+#     def save(dir_name, **kwargs):
+#         save_state_path = os.path.join(dir_name, 'state_dict.pkl')
+#         state = {
+#                     'model': model_nsml.state_dict(),
+#                 }
+#         torch.save(state, save_state_path)
+
+#     def load(dir_name):
+#         save_state_path = os.path.join(dir_name, 'state_dict.pkl')
+#         state = torch.load(save_state_path)
+#         model_nsml.load_state_dict(state['model'])
+        
+#     def infer(test_image_data_path, test_meta_data_path):
+#         # DONOTCHANGE This Line
+#         test_meta_data = pd.read_csv(test_meta_data_path, delimiter=',', header=0)
+        
+#         input_size=224 # you can change this according to your model.
+#         batch_size=100 # you can change this. But when you use 'nsml submit --test' for test infer, there are only 200 number of data.
+#         device = 0
+        
+#         dataloader = DataLoader(
+#                         AIRushDataset(test_image_data_path, test_meta_data, label_path=None,
+#                                       transform=transforms.Compose([transforms.Resize((input_size, input_size)), transforms.ToTensor(),transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])),
+#                         batch_size=batch_size,
+#                         shuffle=False,
+#                         num_workers=0,
+#                         pin_memory=True)
+
+#         nsml.load(checkpoint='20',session='team_62/airush1/185')
+#         model_nsml.to(device)
+#         model_nsml.eval()
+#         predict_list = []
+#         for batch_idx, image in enumerate(dataloader):
+#             image = image.to(device)
+#             output = model_nsml(image).double()
+#             # 여기 수정해서 보기
+#             output_prob = F.softmax(output, dim=1)
+#             predict = np.argmax(to_np(output_prob), axis=1)
+#             predict_list.append(predict)
+#         # model_nsml 을 업데이트 시켜주면 자동 바꿔지지 않을까? 하는 생각
+
+
+#         predict_vector = np.concatenate(predict_list, axis=0)
+#         return predict_vector # this return type should be a numpy array which has shape of (138343)
+
+#     # DONOTCHANGE: They are reserved for nsml
+#     nsml.bind(save=save, load=load, infer=infer)
+
+    
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='parser')
+#     # DONOTCHANGE: They are reserved for nsml
+#     parser.add_argument('--mode', type=str, default='train')
+#     parser.add_argument('--iteration', type=str, default='0')
+#     parser.add_argument('--pause', type=int, default=0)
+    
+#     # custom args
+#     parser.add_argument('--input_size', type=int, default=224)
+#     parser.add_argument('--batch_size', type=int, default=128)
+#     parser.add_argument('--num_workers', type=int, default=8)
+#     parser.add_argument('--gpu_num', type=int, nargs='+', default=[0])
+#     parser.add_argument('--resnet', default=True)
+#     parser.add_argument('--hidden_size', type=int, default=256)
+#     parser.add_argument('--output_size', type=int, default=350) # Fixed
+#     parser.add_argument('--epochs', type=int, default=20)
+#     parser.add_argument('--log_interval', type=int, default=100)
+#     parser.add_argument('--learning_rate', type=float, default=5e-4)
+#     parser.add_argument('--device', type=int, default=0)
+#     parser.add_argument('--seed', type=int, default=42)
+#     args = parser.parse_args()
+
+#     torch.manual_seed(args.seed)
+#     device = args.device
+
+#     if args.resnet:
+#         print("ResNext Model Load")
+#         assert args.input_size == 224
+#         model = resnext50(num_classes=args.output_size)
+#     else:
+#         model = Baseline(args.hidden_size, args.output_size)
+
+#     # DONOTCHANGE: They are reserved for nsml
+#     bind_model(model)
+
+
+#     # below the nsml load
+#     if args.mode == "train":
+#         nsml.load(checkpoint='12',session='team_62/airush1/185')
+#         nsml.save('R')
+#         print('---saved---')
+
+#     if args.pause:
+#         nsml.paused(scope=locals())
+    
+#     print('---end---')
